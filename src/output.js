55abd24585fff8bba3ed8d446e1cf360a563d1fa
Michael Daines <michael@mdaines.com>
2008-07-19 00:07:48 -0700

    make single stretchiness workspace

--- src/output.js
+++ src/output.js
@@ -1,35 +1,47 @@
 Plumb.Output = {
-  
-  output: function(c, parent) {
+  htmlOptions: function(c) {
     var O = Plumb.Output.options;
-    var element = parent.child();
+    var options = {};
     
-    element.id = c.id;
+    options.id = c.id;
     
-    element.className = c.type ? c.type : "box";
-    if (c.root) element.className += " root"
+    options.className = c.type ? c.type : "box";
+    if (c.root) options.className += " root"
     
-    if (c.stretchy) {
-      var subtrahend = (c.width.subtrahend * O.width) + (c.width.subtrahend * O.margin);
-      if (!c.type) subtrahend += O.margin * 2;
-      element.width = c.width.percentage + "% - " + subtrahend + "px";
-    } else {
-      var width = (c.width * O.width) + (c.width * O.margin) + O.margin;
-      if (!c.type) width -= O.margin * 2;
-      element.width = width + "px";
-    }
+    var width = (c.width * O.width) + (c.width * O.margin);
+    if (!c.type) width -= O.margin;
+    if (c.root) width += O.margin;
+    options.width = width + "px";
     
     var left = (c.prepend * O.width) + (c.prepend * O.margin);
     if (!c.type) left += O.margin;
-    element.marginLeft = left + "px";
+    options.marginLeft = left + "px";
       
     var right = (c.append * O.width) + (c.append * O.margin);
-    element.marginRight = right + "px";
+    options.marginRight = right + "px";
+    
+    return options;
+  },
   
+  elementOutput: function(c, parent) {
+    var options = Plumb.Output.htmlOptions(c);
+    
+    parent.className = options.className;
+      
+    parent.setStyle({
+      width: options.width,
+      marginLeft: options.marginLeft,
+      marginRight: options.marginRight
+    });
+    
+    if (!c.type) parent.setStyle({ height: c.height + "px" });
+    
     if (!Object.isUndefined(c.children) && c.children.length > 0) {
-      for (var i = 0; i < c.children.length; i++)
-        arguments.callee(c.children[i], element);
+      for (var i = 0; i < c.children.length; i++) {
+        var child = new Element("div");
+        Plumb.Output.elementOutput(c.children[i], child)
+        parent.insert(child);
+      }
     }
   }
-  
 }
