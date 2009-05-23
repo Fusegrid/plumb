c477b0d5984a3b62bdd0fbaa74d9572c3ed1dcc1
Michael Daines <michael@mdaines.com>
2009-05-23 12:11:45 -0600

    don't output append if the child is also last

--- src/plumb/output.js
+++ src/plumb/output.js
@@ -9,11 +9,10 @@ Plumb.Output = {
       if (child.prepend > 0)
         element.addClassName("prepend-" + child.prepend);
       
-      if (child.append > 0)
-        element.addClassName("append-" + child.append);
-        
       if (index == box.children.length - 1 || box.type == "rows")
         element.addClassName("last");
+      else if (child.append > 0)
+        element.addClassName("append-" + child.append);
         
       if (child.children && child.children.any()) {
         container.insert(element);
