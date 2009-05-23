4bb45f3a189de89828a333d566059faebea92e9a
Michael Daines <michael@mdaines.com>
2009-05-23 11:49:56 -0600

    add back output and recognition

---
+++ src/plumb/output.js
@@ -0,0 +1,29 @@
+Plumb.Output = {
+  
+  output: function(box, container) {
+    box.children.each(function(child, index) {
+      var element = new Element("div");
+      
+      element.addClassName("span-" + child.width);
+      
+      if (child.prepend > 0)
+        element.addClassName("prepend-" + child.prepend);
+      
+      if (child.append > 0)
+        element.addClassName("append-" + child.append);
+        
+      if (index == box.children.length - 1 || box.type == "rows")
+        element.addClassName("last");
+        
+      if (child.children && child.children.any()) {
+        container.insert(element);
+        this.output(child, element);
+      } else {
+        container.insert(element);
+        element.addClassName("column");
+        element.setStyle({ height: child.height + "px" });
+      }
+    }.bind(this));
+  }
+  
+}
